bc2d4e0f05c1a7d74a658ed9174bee73c370dd49
Álvaro Durán Tovar <hermesdt@gmail.com>
2014-02-07 11:56:18 +0100

    Added planoesquematico

--- app.js
+++ app.js
@@ -4,8 +4,6 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
@@ -20,7 +18,7 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
-app.use(app.router);
+// app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
@@ -28,8 +26,6 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
