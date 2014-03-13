5544e6fc79abd945ec0cecfcda085bdb2760d0bf
Álvaro Durán Tovar <hermesdt@gmail.com>
2014-03-13 23:58:18 +0100

    .

--- scripts/node.rb
+++ scripts/node.rb
@@ -1,6 +1,6 @@
 require 'json'
 require 'net/http'
+require 'active_support/core_ext/hash'
 
 resp = Net::HTTP.get_response URI.parse('https://servicios.emtmadrid.es:8443/bus/servicebus.asmx/GetNodesLines?idClient=EMT.SERVICIOS.HERMES.CAVERN&passKey=32D1C2C1-F3C5-411E-A4A0-1B913C9F411D&Nodes=')
-puts resp.body
-
+puts Hash.from_xml(resp.body).to_json
